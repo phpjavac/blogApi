e89150680f1d6370317ce459a2111fc11ca869ea
yiziluoying <1060854705@qq.om>
2019-07-26 14:43:22 +0800

    修改跨域

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 // Listen the server
+//设置允许跨域访问该服务.
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3093');
+  //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
 app.listen(PORT, HOST, () =>
   console.log(`Server is listening on http://${HOST}:${PORT}`)
 );
