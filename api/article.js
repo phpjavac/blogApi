b6e36cf281eeb0505a2903166b1966586b6a732b
yiziluoying <1060854705@qq.om>
2019-07-30 15:24:58 +0800

    创建文章

--- api/article.js
+++ api/article.js
@@ -1,6 +1,62 @@
 var express = require('express');
 var router = express.Router();
-var { Tag } = require('../models/article')
+var { Tag, Article } = require('../models/article')
+
+router.get('/list/:page/:pageSize', (req, res) => {
+    console.log(req.params)
+    const page = +req.params.page || 1
+    const pageSize = +req.params.pageSize || 10
+    var query = Article.find({}).sort({"time":-1});
+    query.skip((page - 1) * pageSize);
+    query.limit(pageSize);
+    //计算分页数据
+    query.exec(function (err, rs) {
+        if (err) {
+            res.send(err);
+        } else {
+            //计算数据总数
+            Article.find(function (err, result) {
+                jsonArray = { data: { list: rs }, page: page, pageSize: pageSize, total: result.length };
+                res.json(jsonArray);
+            });
+
+        }
+    });
+    // Article.find({}, (error, body) => {
+    //     const data = {
+    //         code: 0,
+    //         data: {
+    //             list:body
+    //         },
+    //         message: ""
+    //     }
+    //     res.send(data);
+    // })
+})
+
+router.post('/create', (req, res) => {
+    const article = new Article()
+    article.title = req.body.title || ""
+    article.code = req.body.code || ""
+    article.tag = req.body.tag || []
+    article.summary = req.body.summary || ""
+    article.content = req.body.content || ""
+    article.imgurl = req.body.imgurl || ""
+    article.state = req.body.state || true
+    article.time = req.body.imgurl || new Date().getTime()
+
+
+    article.save(err => {
+        if (err) {
+            res.send(400, err);
+            return
+        }
+        res.json({ message: '新建文章成功！' });
+        return
+    })
+
+})
+
 router.post('/addtag', (req, res) => {
     var tag = new Tag();
     const tagName = req.body.tagName || ""
@@ -35,7 +91,7 @@ router.delete('/deletetag/:id', (req, res) => {
             Tag.find({}, (err, data) => {
                 res.send({
                     message: '删除成功',
-                    list:data
+                    list: data
                 })
             })
 
--- models/article.js
+++ models/article.js
@@ -7,7 +7,36 @@ const TagSchema = new mongoose.Schema({
         type: String,
         unique: true
     }
-},{versionKey: false})
-
+}, { versionKey: false })
+// 建立文章表
+const ArticleSchema = new mongoose.Schema({
+    code: {
+        type: String
+    },
+    title: {
+        type: String
+    },
+    tag: {
+        type: Array
+    },
+    summary: {
+        type: String
+    },
+    content: {
+        type: String
+    },
+    imgurl: {
+        type: String
+    },
+    time: {
+        type: String,
+        default: new Date().getTime()
+    },
+    state: {
+        type: Boolean,
+        default: true
+    }
+}, { versionKey: false })
 // 建立标签数据库模型
 module.exports.Tag = mongoose.model('Tag', TagSchema)
+module.exports.Article = mongoose.model('Article', ArticleSchema)
